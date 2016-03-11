395f2865832d7f29ca943208171afbdbeb70a97a
Otavi0 <olopes@mundipagg.com>
2016-03-11 10:07:33 -0300

    Atualiza a versão
    Versão que contem o recurso Buyer e os novos métodos do recurso CreditCard

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'stoneEcommercePython
 
 setup(
     name='stone_ecommerce_python',
-    version='1.0.0',
+    version='1.0.1',
     package_dir={'stoneEcommercePython': 'stoneEcommercePython', 'data_contracts': 'stoneEcommercePython/data_contracts', 'enum_types': 'stoneEcommercePython/enum_types', 'resource_clients': 'stoneEcommercePython/resource_clients', 'transaction_report_file': 'stoneEcommercePython/transaction_report_file'},
     packages=['stoneEcommercePython', 'data_contracts', 'enum_types', 'resource_clients', 'transaction_report_file'],
     url='https://github.com/stone-pagamentos/stone-ecommerce-python',
